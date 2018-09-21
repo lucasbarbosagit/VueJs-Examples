c87279ee11f404438b9f3d68f4c400426618b8e8
Lucas Barbosa <lucas.barbosa.work@hotmail.com>
2018-09-21 14:02:10 -0300

    vuex simple example

---
+++ Vuex Simple Example/src/components/Stats.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="stats">
+    <h1>Contador de Links</h1>
+    <p>Temos atualmente {{ countLinks }} links</p>
+
+    <button v-on:click="removeAllLinks">Remover todos os links</button>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'Stats',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'countLinks'
+    ]),
+  },
+  methods: {
+    ...mapMutations(['REMOVE_ALL']),
+    ...mapActions(['removeAll']),
+    removeAllLinks() {
+      this.removeAll().then(() => {
+        this.msg = 'Todos foram removidos'
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+}
+</style>
